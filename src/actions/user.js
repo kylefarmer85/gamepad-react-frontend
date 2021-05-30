867a6a9f7ad3a5dc76c4f41598558092213b081a
kylefarmer85 <kylefarmer85@gmail.com>
2021-05-30 16:02:12 -0500

    install axios, change fetchUser action to loginUser, use async/await

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,67 +1,59 @@
 import history from '../history';
 import { toast } from 'react-toastify';
 import API from '../API';
+import axios from 'axios';
 
 export const logoutUser = () => {
   return {
-    type: 'LOGOUT_USER',
+    type: 'LOGOUT_USER'
   };
 };
 
-export const currentUser = (data) => {
+export const currentUser = data => {
   return {
     type: 'CURRENT_USER',
-    data,
+    data
   };
 };
 
 export const startAddingUserRequest = () => {
   return {
-    type: 'START_ADDING_USER_REQUEST',
+    type: 'START_ADDING_USER_REQUEST'
   };
 };
 
-export function fetchUser(userObj) {
-  return (dispatch) => {
-    dispatch({ type: 'START_ADDING_USER_REQUEST' });
+export const loginUser = (username, password) => {
+  return async dispatch => {
+    try {
+      dispatch({ type: 'START_ADDING_USER_REQUEST' });
 
-    const reqObj = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: userObj.username,
-        password: userObj.password,
-      }),
-    };
-
-    fetch(`${API}/api/v1/auth`, reqObj)
-      .then((resp) => resp.json())
-      .then((data) => {
-        
-        if (data.error) {
-          toast.error("Invalid Username/Password", { position: 'top-center', autoClose: 5000 });
-
-          history.push('/login');
-
-        } else {
-          dispatch({ type: 'LOGIN_USER', data });
-          localStorage.setItem('my_app_token', data.token);
-
-          toast.success(`Welcome ${data.user.username}!`, {
-            position: 'bottom-center',
-            autoClose: 3000,
-          });
-
-          history.push('/');
-        }
+      const { data } = await axios.post(`${API}/api/v1/auth`, {
+        username,
+        password
       });
+
+      dispatch({ type: 'LOGIN_USER', data });
+      localStorage.setItem('my_app_token', data.token);
+
+      toast.success(`Welcome ${data.user.username}!`, {
+        position: 'bottom-center',
+        autoClose: 3000
+      });
+      history.push('/');
+      
+    } catch (error) {
+      toast.error('Invalid Username/Password', {
+        position: 'top-center',
+        autoClose: 3000
+      });
+
+      history.push('/login');
+    }
   };
-}
+};
 
 export function signupUser(userObj) {
-  return (dispatch) => {
+  return dispatch => {
     dispatch({ type: 'START_ADDING_USER_REQUEST' });
 
     const formData = new FormData();
@@ -73,14 +65,17 @@ export function signupUser(userObj) {
     const reqObj = {
       method: 'POST',
       headers: {},
-      body: formData,
+      body: formData
     };
 
     fetch(`${API}/api/v1/users`, reqObj)
-      .then((resp) => resp.json())
-      .then((data) => {
+      .then(resp => resp.json())
+      .then(data => {
         if (data.error) {
-          toast.error("Unable to create user.", { position: 'top-center', autoClose: 3000 });
+          toast.error('Unable to create user.', {
+            position: 'top-center',
+            autoClose: 3000
+          });
 
           history.push('/signup');
         } else {
@@ -89,7 +84,7 @@ export function signupUser(userObj) {
 
           toast.success(`Welcome ${data.user.username}!`, {
             position: 'bottom-center',
-            autoClose: 3000,
+            autoClose: 3000
           });
 
           history.push('/');
@@ -99,7 +94,7 @@ export function signupUser(userObj) {
 }
 
 export function updateUser(userObj) {
-  return (dispatch) => {
+  return dispatch => {
     const formData = new FormData();
 
     Object.keys(userObj).forEach((key, value) => {
@@ -109,21 +104,23 @@ export function updateUser(userObj) {
     const reqObj = {
       method: 'PATCH',
       headers: {},
-      body: formData,
+      body: formData
     };
 
     fetch(`${API}/api/v1/users/${userObj.id}`, reqObj)
-      .then((resp) => resp.json())
-      .then((data) => {
+      .then(resp => resp.json())
+      .then(data => {
         if (data.error) {
-          toast.error("User not updated", { position: 'top-center', autoClose: 5000 });
+          toast.error('User not updated', {
+            position: 'top-center',
+            autoClose: 5000
+          });
 
           history.push(`/users/${userObj.id}/profile`);
-
         } else {
           toast.info(`${userObj.username}'s info was updated!`, {
             position: 'bottom-center',
-            autoClose: 3000,
+            autoClose: 3000
           });
 
           dispatch({ type: 'UPDATE_USER', data });
