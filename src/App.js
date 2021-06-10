a4ee68f1ffea4b444b6d1debabffd366cd546c59
kylefarmer85 <kylefarmer85@gmail.com>
2021-06-09 22:50:32 -0500

    App.js converted to functional component, working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import { Switch, Route } from 'react-router-dom';
 import {
   currentUser,
   logoutUser,
@@ -20,67 +21,58 @@ import ReviewsPage from './components/reviewsPage/ReviewsPage';
 import ReviewShow from './components/ReviewPage';
 import API from './API';
 
-class App extends Component {
-  componentDidMount() {
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
     const token = localStorage.getItem('my_app_token');
 
     if (!token) {
       return;
     } else {
-      this.props.startAddingUserRequest();
-
+      dispatch(startAddingUserRequest());
       const reqObj = {
         method: 'GET',
         headers: {
           Authorization: `Bearer ${token}`
         }
       };
-
-      fetch(`${API}/api/v1/current_user`, reqObj)
-        .then(resp => resp.json())
-        .then(data => {
-          if (data.error) {
-            localStorage.removeItem('my_app_token');
-            this.props.logoutUser();
-          } else {
-            this.props.currentUser(data);
-          }
-        });
+      try {
+        const { data } = await axios.get(`${API}/api/v1/current_user`, reqObj);
+        dispatch(currentUser(data));
+      } catch (error) {
+        localStorage.removeItem('my_app_token');
+        dispatch(logoutUser());
+      }
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className='App'>
-        <NavBar />
-        <NavConsoleList />
-        <Switch>
-          <Route exact path='/signup' component={Signup} />
-          <Route exact path='/login' component={Login} />
-          <Route
-            exact
-            path='/games/search/:searchTerm'
-            component={GameSearchResultsPage}
-          />
-          <Route exact path='/games/:id' component={GamePage} />
-          <Route exact path='/reviews' component={ReviewsPage} />
-          <Route exact path='/reviews/:id' component={ReviewShow} />
-          <Route exact path='/users/:id/profile' component={ProfilePage} />
-          <Route exact path='/users/:id/edit' component={EditUserInfo} />
-          <Route
-            exact
-            path='/users/search/:searchTerm'
-            component={UserSearchResultsPage}
-          />
-          <Route path='/' component={HomePage} />
-        </Switch>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='App'>
+      <NavBar />
+      <NavConsoleList />
+      <Switch>
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/login' component={Login} />
+        <Route
+          exact
+          path='/games/search/:searchTerm'
+          component={GameSearchResultsPage}
+        />
+        <Route exact path='/games/:id' component={GamePage} />
+        <Route exact path='/reviews' component={ReviewsPage} />
+        <Route exact path='/reviews/:id' component={ReviewShow} />
+        <Route exact path='/users/:id/profile' component={ProfilePage} />
+        <Route exact path='/users/:id/edit' component={EditUserInfo} />
+        <Route
+          exact
+          path='/users/search/:searchTerm'
+          component={UserSearchResultsPage}
+        />
+        <Route path='/' component={HomePage} />
+      </Switch>
+    </div>
+  );
+};
 
-export default connect(null, {
-  currentUser,
-  startAddingUserRequest,
-  logoutUser
-})(withRouter(App));
+export default App;
