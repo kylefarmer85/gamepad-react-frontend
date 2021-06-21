45b1bcff26d1a7361dd3c94a64b25601dcf7d33e
kylefarmer85 <kylefarmer85@gmail.com>
2021-06-20 19:27:23 -0500

    convert HighestRatedByFollowingsContainer from class to functional comp

--- src/components/homePage/HighestRatedByFollowingsContainer.js
+++ src/components/homePage/HighestRatedByFollowingsContainer.js
@@ -1,104 +1,73 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import SlicedGamesContainer from './SlicedGamesContainer';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import { connect } from 'react-redux';
 import API from '../../API';
+import axios from 'axios';
 
-class HighestRatedByFollowingsContainer extends Component {
-  state = {
-    games: [],
-    index: 0,
-    loading: true
-  };
+const HighestRatedByFollowingsContainer = () => {
+  const [games, setGames] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
-    this.fetchGames();
-  }
+  const id = useSelector(state => state.user.id);
 
-  fetchGames = () => {
-    const reqObj = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        id: this.props.user.id
-      })
+  useEffect(() => {
+    const fetchGames = async () => {
+      const { data } = await axios.post(
+        `${API}/api/v1/games/highestratedbyfollowings`,
+        {
+          id
+        }
+      );
+      setGames(data);
+      setLoading(false);
     };
 
-    fetch(`${API}/api/v1/games/highestratedbyfollowings`, reqObj)
-      .then(resp => resp.json())
-      .then(data => {
-        if (data.error) {
-          alert(data.error);
-        } else {
-          this.setState({
-            games: data,
-            loading: false
-          });
-        }
-      });
+    fetchGames();
+  }, [id]);
+
+  const slicedGames = () => {
+    return index > games.length ? setIndex(0) : games.slice(index, index + 4);
   };
 
-  slicedGames = () => {
-    if (this.state.index > this.state.games.length) {
-      this.setState({
-        index: 0
-      });
-    }
-    return this.state.games.slice(this.state.index, this.state.index + 4);
+  const nextGames = () => {
+    setIndex(prevIndex => prevIndex + 4);
   };
 
-  nextGames = () => {
-    this.setState(prevState => {
-      return {
-        index: prevState.index + 4
-      };
-    });
-  };
-
-  render() {
-    return (
-      <Container fluid className='mt-4 text-center'>
-        <Row className='align-items-center'>
-          <Col lg={3}>
-            <label>Highest Rated Games by Users You Follow</label>
-            <br />
-            <br />
-            <button
-              type='button'
-              className='btn-nes secondary'
-              onClick={this.nextGames}
-            >
-              more{'>'}
-            </button>
-          </Col>
-          <Col lg={9}>
-            {this.state.loading ? null : (
-              <div className='d-flex flex-wrap justify-content-around slide-in'>
-                {this.state.games.length ? (
-                  <SlicedGamesContainer slicedGames={this.slicedGames()} />
-                ) : (
-                  <p>Follow users to see their highest rated games.</p>
-                )}
-              </div>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    user: state.user
-  };
+  return (
+    <Container fluid className='mt-4 text-center'>
+      <Row className='align-items-center'>
+        <Col lg={3}>
+          <label>Highest Rated Games by Users You Follow</label>
+          <br />
+          <br />
+          <button
+            type='button'
+            className='btn-nes secondary'
+            onClick={nextGames}
+          >
+            Next Games {'>'}
+          </button>
+        </Col>
+        <Col lg={9}>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <div className='d-flex flex-wrap justify-content-around slide-in'>
+              {games.length ? (
+                <SlicedGamesContainer slicedGames={slicedGames()} />
+              ) : (
+                <p>Follow users to see their highest rated games.</p>
+              )}
+            </div>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
-export default connect(
-  mapStateToProps,
-  null
-)(HighestRatedByFollowingsContainer);
+export default HighestRatedByFollowingsContainer;
