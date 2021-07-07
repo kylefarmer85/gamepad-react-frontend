7110f2ef23802e86a2e7759bd8d5daa167f0d5d1
kylefarmer85 <kylefarmer85@gmail.com>
2021-07-06 21:27:13 -0500

    convert GamePage from class to func comp, add axios

--- src/components/gamePage/GamePage.js
+++ src/components/gamePage/GamePage.js
@@ -1,52 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Loading from '../reusable/Loading';
 import GameShowContainer from './GameShowContainer';
+import axios from 'axios';
 import API from '../../API';
 
-class GamePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      gameObj: {},
-      screenshots: {},
-      gameApiId: this.props.match.params.id
-    };
-  }
+const GamePage = ({ match }) => {
+  const [loading, setLoading] = useState(true);
+  const [gameObj, setGameObj] = useState({});
+  const [screenshots, setScreenshots] = useState({});
 
-  componentDidMount() {
-    fetch(`${API}/api/v1/games/${this.state.gameApiId}`)
-      .then(resp => resp.json())
-      .then(game => {
-        if (game.error) {
-          this.props.history.push('/home');
-          alert(game.error);
-        } else {
-          this.setState({
-            gameObj: game.game_obj,
-            screenshots: game.game_ss.results,
-            loading: false
-          });
-        }
-      });
-  }
+  let history = useHistory();
 
-  render() {
-    return (
-      <div>
-        {this.state.loading ? (
-          <div style={{ marginTop: '7%' }}>
-            <Loading />
-          </div>
-        ) : (
-          <GameShowContainer
-            game={this.state.gameObj}
-            screenshots={this.state.screenshots}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    try {
+      const fetchGame = async () => {
+        const { data } = await axios.get(
+          `${API}/api/v1/games/${match.params.id}`
+        );
+        setGameObj(data.game_obj);
+        setScreenshots(data.game_ss.results);
+        setLoading(false);
+      };
+      fetchGame();
+    } catch (error) {
+      history.push('/home');
+      alert(error);
+    }
+  }, [match, history]);
+
+  return (
+    <div>
+      {loading ? (
+        <div style={{ marginTop: '7%' }}>
+          <Loading />
+        </div>
+      ) : (
+        <GameShowContainer game={gameObj} screenshots={screenshots} />
+      )}
+    </div>
+  );
+};
 
 export default GamePage;
